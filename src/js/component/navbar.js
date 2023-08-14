4107122d5ab189f6f72a2cd65238c65c4c8ec9b2
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-08-14 18:25:03 -0400

    se quitaron los onclick del tablero del jugador ya que no se necesitan

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,16 +4,9 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Battleship</span>
+			<Link className="ms-3" to="/">
+				<span className="navbar-brand mb-0 h1">Battleship (click here to return to home)</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">
-						Check the Context in action
-					</button>
-				</Link>
-			</div>
 		</nav>
 	);
 };
--- src/js/views/game.js
+++ src/js/views/game.js
@@ -54,11 +54,13 @@ export const Game = () => {
 	return (
 		<div className="container-fluid border border-dark m-0 p-0">
 			<div className="text-center">
-				{isPlayerTurn ? <h1>It's {store.playerName} turn</h1> : <h1>It's CPU turn</h1>}
-				<span className="text-primary">A blue box represents an empty space</span> <br />
-				<span className="text-secondary">A grey box represents a piece of a ship</span> <br />
-				<span className="text-danger">A red box represents a ship that has been hit</span> <br />
-				<span>A white box represents a missed shot</span>
+				{isPlayerTurn ? <h1>It's {store.playerName} turn</h1> : <h1>It's CPU turn. </h1>}
+				<h5>
+					<span className="text-primary">A blue box represents an empty space. ||</span>
+					<span className="text-secondary">A grey box represents a piece of a ship. ||</span>
+					<span className="text-danger">A red box represents a ship that has been hit. ||</span>
+					<span>A white box represents a missed shot.</span>
+				</h5>
 			</div>
 			<div className="container">
 				<div className="row">
@@ -75,23 +77,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">A</div>
 								{store.playerBoard[0].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[0][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[0][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[0][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										A{index + 1}
 									</button>
 								))}
@@ -99,23 +85,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">B</div>
 								{store.playerBoard[1].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[1][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[1][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[1][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										B{index + 1}
 									</button>
 								))}
@@ -123,23 +93,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">C</div>
 								{store.playerBoard[2].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[2][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[2][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[2][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										C{index + 1}
 									</button>
 								))}
@@ -147,23 +101,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">D</div>
 								{store.playerBoard[3].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[3][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[3][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[3][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										D{index + 1}
 									</button>
 								))}
@@ -171,23 +109,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">E</div>
 								{store.playerBoard[4].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[4][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[4][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[4][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										E{index + 1}
 									</button>
 								))}
@@ -195,23 +117,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">F</div>
 								{store.playerBoard[5].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[5][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[5][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[5][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										F{index + 1}
 									</button>
 								))}
@@ -219,23 +125,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">G</div>
 								{store.playerBoard[6].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[6][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[6][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[6][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										G{index + 1}
 									</button>
 								))}
@@ -243,23 +133,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">H</div>
 								{store.playerBoard[7].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[7][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[7][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[7][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										H{index + 1}
 									</button>
 								))}
@@ -267,23 +141,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">I</div>
 								{store.playerBoard[8].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[8][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[8][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[8][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										I{index + 1}
 									</button>
 								))}
@@ -291,23 +149,7 @@ export const Game = () => {
 							<div className="fitY">
 								<div className="infoY">J</div>
 								{store.playerBoard[9].map((item, index) => (
-									<button
-										className={actions.classManagerPlayer(item)}
-										key={index}
-										value={item}
-										onClick={(e) => {
-											if (e.target.value == 1) {
-												isPlayerBoard[9][index] = 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else if (e.target.value == 2) {
-												isPlayerBoard[9][index] == 2;
-												actions.updatePlayer(isPlayerBoard);
-											} else {
-												isPlayerBoard[9][index] = 3;
-												actions.updatePlayer(isPlayerBoard);
-											}
-										}}
-									>
+									<button className={actions.classManagerPlayer(item)} key={index} value={item}>
 										J{index + 1}
 									</button>
 								))}
